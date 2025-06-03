55cb476fb28314c2aab75d0209c24f00db36baca
Madison Kolley <53013984+Meowitsmadi@users.noreply.github.com>
2025-06-03 02:56:38 -0700

    create album carousel on dashboard

--- vite_frontend/src/components/Dashboard.jsx
+++ vite_frontend/src/components/Dashboard.jsx
@@ -3,11 +3,22 @@ import { useNavigate, Link } from 'react-router-dom';
 import HomeNavbar from "./HomeNavbar";
 import "./styling/Dashboard.css"
 import { getAlbums } from '../utils/albums';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "../slick-theme.css";
 
 
 const Dashboard = () => {
   const [albums, setAlbums] = useState([]);
   const navigate = useNavigate();
+  const settings = {
+    dots: true,
+    arrows: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+  };
 
   const handleNewAlbumClick = () => {
     navigate("/create-album");
@@ -29,31 +40,27 @@ const Dashboard = () => {
     <div>
       <HomeNavbar />
       <button onClick={handleNewAlbumClick}>Create New Album</button>
-      <div>
-      <h2>Albums</h2>
-      </div>
-      <div className="album-card-container">
-        {albums.length > 0 ? (
-          albums.map((album) => (
-            <Link to={`/albums/${album.id}/pages/`} key={album.id}>
-              <div className="album-card">
-                {album.cover_img ? (
-                  <img
-                    src={`http://127.0.0.1:8000${album.cover_img}`} // Use the full URL to the media file
-                    alt={`${album.name} cover`}
-                    style={{ width: "150px", height: "150px", objectFit: "cover" }}
-                  />
-                ) : (
-                  <p>No cover image</p>
-                )}
-                <div className="album-card-title">{album.name}</div> 
-                Created on {new Date(album.date_created).toLocaleDateString()}
-              </div>
-            </Link>
-          ))
-        ) : (
-          <p>No albums found. Create your first album!</p>
-      )}
+      <div className="album-carousel">
+        <Slider {...settings}>
+            {albums.length > 0 ? (
+              albums.map((album) => (
+                <Link to={`/albums/${album.id}/pages/`} key={album.id} className="album-card">
+                    {album.cover_img ? (
+                      <img
+                        src={`http://127.0.0.1:8000${album.cover_img}`} // Use the full URL to the media file
+                        alt={`${album.name} cover`}
+                      />
+                    ) : (
+                      <p>No cover image</p>
+                    )}
+                    <div className="album-card-title">{album.name}</div> 
+                    Created on {new Date(album.date_created).toLocaleDateString()}
+                </Link>
+              ))
+            ) : (
+              <p>No albums found. Create your first album!</p>
+            )}
+        </Slider>
       </div>
     </div>
   );
